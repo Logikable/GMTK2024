2a4bda28d9ccd44008295bf28cf9fc7f7f6529b6
ellie <ellinorzetterman@gmail.com>
2024-08-18 12:45:03 +0200

    Add scale tween to draggable.gd

--- scripts/draggable.gd
+++ scripts/draggable.gd
@@ -31,13 +31,35 @@ func _process(delta: float) -> void:
 func _input(event) -> void:
   if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
     var parent = self.get_parent()
+    
     if event.pressed:
       # Things are computed using global rather than local position.
       if Util.global_rect(parent).has_point(event.position):
         held = true
         click_pos = event.position - parent.position * Util.global_scale(parent)
         initial_pos = parent.position
-    else:     # This was a release event.
+
+        # We need to create a tween to animate the nodes we're dragging
+
+        # We do this to make sure the node we're dragging scales from its center
+        parent.pivot_offset = parent.size/2
+        
+        # Create the tween
+        var tween = create_tween()
+
+        # Tween the scale value so stuff shrinks a little when you grab it
+        tween.tween_property(parent, "scale", Vector2(0.95, 0.95), 0.1).set_trans(Tween.TRANS_ELASTIC)
+
+        # Something is wrong here... Not sure what's going on
+        # To repro: Drag a card to the right side of the screen and click+hold it. It does a little jump :(
+        # The jump happens even when the card is on the left side of the screen, it's just much smaller
+        # If you change the scale tween to be more drastic, the effect is bigger
+        
+
+    # We use the release event to scale back cards after dragging
+    else:
+      var tween = create_tween()
+      tween.tween_property(parent, "scale", Vector2(1, 1), 0.1).set_trans(Tween.TRANS_ELASTIC)
       if held:  # I only care if I was the object being held.
         held = false
         released.emit(parent, initial_pos)
